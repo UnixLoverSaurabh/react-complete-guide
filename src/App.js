23e474b9faff8cc302931989790f31be3b654c0d
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-05-28 15:16:34 +0530

    setState() for state manipulation

--- src/App.js
+++ src/App.js
@@ -9,7 +9,20 @@ class App extends Component {
       {name : "saurabh", age : 24},
       {name : "Shubham", age : 26},
       {name : "Sunil", age : 45}
-    ]
+    ],
+    otherState : "This will not be altered"
+  }
+
+  switchNameHandler = () => {
+    // DON'T DO THIS : this.state.persons[0].name = 'Maximilian';
+
+    this.setState({
+      persons : [
+        {name : "Gaurabh", age : 23},
+        {name : "Shubham Kumar", age : 26},
+        {name : "Sunil Prasad", age : 46}
+      ]
+    });
   }
 
   render() {
@@ -19,9 +32,11 @@ class App extends Component {
           <h1>Welcome to React</h1>
             <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
 
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}> This is children under third person </Person>
+            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}> This is children under third person </Person>
 
-            <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+            <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+
+            <button onClick={this.switchNameHandler}>Switch name</button>
         </header>
       </div>
     );
