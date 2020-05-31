90fba0c9608c41f0d6313554baa319cb07ccc7c9
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-05-31 19:26:06 +0530

    debug only in developement phase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary.js'
 
 class App extends Component {
   state = {
@@ -34,7 +35,9 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age} key={person.id}/>
+            return <ErrorBoundary key={person.id}>
+                    <Person click={() => this.deletePersonHandler(index)} name={person.name} age={person.age}/>
+                  </ErrorBoundary>
           })}
         </div>
       );
